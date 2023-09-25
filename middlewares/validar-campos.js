ca87dcfcc06a7979390768411b3e8aa719b75d71
Pablo Sarmiento <stilar.sj@gmail.com>
2023-09-25 17:07:11 -0300

    backend 1.0 finalizado

--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -4,9 +4,7 @@ const {validationResult} = require("express-validator")
 const validarCampos = (req, res= response, next)=>{
 
     const errors = validationResult(req);
-    console.log(errors);
     if(!errors.isEmpty()){
-        console.log(errors.mapped());
         return res.status(400).json(
             {
                 "ok": false,
