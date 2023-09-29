a574b83ae6b58488824c6e8b878fe88c67f9c9eb
Pablo Sarmiento <stilar.sj@gmail.com>
2023-09-29 17:22:34 -0300

    backend 1.0 finalizado

--- middlewares/validar-campos.js
+++ middlewares/validar-campos.js
@@ -5,6 +5,7 @@ const validarCampos = (req, res= response, next)=>{
 
     const errors = validationResult(req);
     if(!errors.isEmpty()){
+        
         return res.status(400).json(
             {
                 "ok": false,
